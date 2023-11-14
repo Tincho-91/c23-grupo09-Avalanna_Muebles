5c35afe0807d448b2df8bfdad9983bab1a8d606f
Tincho-91 <martin_baryuk@outlook.com>
2023-11-13 23:19:15 -0300

    creando base de login

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
    return res.sendFile(path.join(__dirname, "/views/productDetail.html"));
 })
 
+app.get("/login", (req, res) => {
+   return res.sendFile(path.join(__dirname, "/views/login.html"));
+})
+
 app.listen(3000, () => console.log("corriendo en el puerto 3000"));
 
 app.use(express.static("public"));
