5a98225ee58e9ab7710a5eae0d6946599740795c
flavia1504 <milhouse.mb@gmail.com>
2023-11-22 19:08:54 -0300

    cambios

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = 3030;
+const PORT = 3000;
 
 app.get("/", (req, res) => {
    return res.sendFile(path.join(__dirname, "/views/index.html"));
