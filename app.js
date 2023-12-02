eb3f9d3c0aeafac1b21f82174946270571237d19
MelanyStrack <melany.strack@gmail.com>
2023-12-02 20:19:42 -0300

    CORRECCIONES

--- app.js
+++ app.js
@@ -28,11 +28,6 @@ app.get("/productCart", (req, res) => {
 })
 
 
-app.get("/", (req, res) => {
-   return res.sendFile(path.join(__dirname, "/views/header.html"));
-})
-
-
 app.listen(3000, () => console.log("corriendo en el puerto 3000"));
 
 
