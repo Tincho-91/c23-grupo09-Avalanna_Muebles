bb69beea68bdde533ef3ab24c19abd619770c2c0
MelanyStrack <melany.strack@gmail.com>
2023-12-17 20:31:02 -0300

    Modifiqué el archivo products.json para que sea mas dinamico

--- avalanna/src/controllers/productsController.js
+++ avalanna/src/controllers/productsController.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 const path = require("path");
-const json = path.join(__dirname, "../../data","/products.json");
+const {getJson, setJson} = require("../utility/jsonMethod")
+
 
 const productsController = {
     detail:(req,res) => {
         const id = req.params.id;        
-        const productsJson = fs.readFileSync(json,"utf-8");
-        const products = JSON.parse(productsJson);
+        const products = getJson("products.json")
         const product = products.find(elemento => elemento.id == id);
         res.render("products/productDetail", {title: product.name, product})
     },
@@ -24,8 +24,7 @@ const productsController = {
     },
     dashboard:(req,res) => {
         const propiedades = ["id", "image", "name", "price"];
-        const productsJson = fs.readFileSync(json,"utf-8");
-        const products = JSON.parse(productsJson);
+        const products = getJson("products.json")
         res.render("products/dashboard", {title: "Dashboard", products, propiedades})
     }
 }
