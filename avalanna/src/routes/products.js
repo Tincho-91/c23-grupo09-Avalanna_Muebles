bdb1c7b98c5a80337ce5c1305e09e1499928f998
flavia1504 <milhouse.mb@gmail.com>
2023-12-31 15:33:50 -0300

    ....

--- avalanna/src/routes/products.js
+++ avalanna/src/routes/products.js
@@ -8,6 +8,7 @@ const productController = require("../controllers/productsController")
 router.get('/detail/:id', productController.detail);
 router.get('/formCreate', productController.formulario)
 router.get('/productCart', productController.cart)
+
 router.get('/formEdit/:id', productController.edform)
 router.put('/formEdit/:id', productController.update)
 
