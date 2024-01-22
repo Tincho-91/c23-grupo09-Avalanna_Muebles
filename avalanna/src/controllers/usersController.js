0eae5cd11321634a7e0c57db6778eb99ebfd46da
Tincho-91 <martin_baryuk@outlook.com>
2024-01-21 22:27:02 -0300

    vista y lógica de actualización de usuarios terminada

--- avalanna/src/controllers/usersController.js
+++ avalanna/src/controllers/usersController.js
@@ -1,3 +1,4 @@
+const {setJson,getJson} = require("../utility/jsonMethod");
 const userController = {
     login: (req,res)=>{
         res.render("users/login", {title:"Ingresar"});
@@ -5,6 +6,41 @@ const userController = {
     register:(req,res)=>{
         res.render("users/register", {title:"Registrarme"});
     },
-}
-
+    edform:(req,res)=>{
+        const {id} = req.params;
+        const users = getJson("users.json");
+        const user = users.find(elemento => elemento.id == id);
+        res.render('./users/actualizar-datos-usuario', { title: 'Editar', user, usuario:req.session.user});
+      },
+      update:(req,res)=>{
+        const {id} = req.params;
+        const {NameAndSurname,email,age,tel,date,rol} = req.body;
+        const users = getJson("users.json");
+        const usuarios = users.map(element => {
+          if (element.id == id) {
+            return {
+              id,
+              NameAndSurname,
+              email,
+              tel,
+              age,
+              date,
+              image: req.file ? req.file.filename : element.image, 
+              password: element.password,
+              rol: rol ? rol : "user"
+            }
+          }
+          return element
+        });
+        setJson(usuarios,"users.json");
+        const userUpdate = usuarios.find(elemento => elemento.id == id);
+        req.session.user = userUpdate;
+        delete userUpdate.password
+        res.cookie('user',(userUpdate))
+        res.redirect(`/users/editar/${id}`);
+      },
+      dashboard:(req,res)=>{
+        res.send(req.session.user)
+      }
+  }
 module.exports = userController;
--- avalanna/src/routes/users.js
+++ avalanna/src/routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 const userController = require('../controllers/usersController');
+const upload = require('../validations/uploadUser');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/ingresar', userController.login);
 router.get('/registrarme', userController.register);
+
+router.get('/editar/:id', userController.edform)
+router.put('/editar/:id',upload.single('image'), userController.update)
 
 module.exports = router;
