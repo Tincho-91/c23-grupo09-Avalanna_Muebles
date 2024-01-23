2f819207d7a6e683ea6949905d5145a638e64400
Tincho-91 <martin_baryuk@outlook.com>
2024-01-23 02:17:42 -0300

    resolviendo conflictos

--- avalanna/src/validations/loginValidator.js
+++ avalanna/src/validations/loginValidator.js
@@ -4,15 +4,15 @@ const bcrypt = require('bcryptjs');
 const users= getJson('users.json')
 
 module.exports = [
-    body('email').notEmpty().withMessage('el campo no puede estar vacio').bail()
-    .isEmail().withMessage('El valor ingresado debe tener el formato de un correo electronico').bail()
+    body('email').notEmpty().withMessage('El campo no puede estar vacío').bail()
+    .isEmail().withMessage('El valor ingresado debe tener el formato de un correo electrónico').bail()
     .custom(value => { 
         const user = users.find(elemento => elemento.email == value);
         return user ? true : false
-    }).withMessage("el usuario no existe"),
-    body("password1").notEmpty().withMessage("el campo no puede estar vacio").bail()
+    }).withMessage("El usuario no existe"),
+    body("password1").notEmpty().withMessage("El campo no puede estar vacío").bail()
     .custom((value,{req})=>{
         const user = users.find(elemento => elemento.email == req.body.email)
         return bcrypt.compareSync(value, user.password);
-    }).withMessage("la contraseña no es correcta")
+    }).withMessage("La contraseña no es correcta")
 ]
