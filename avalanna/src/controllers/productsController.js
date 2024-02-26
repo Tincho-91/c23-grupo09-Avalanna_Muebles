b94ecd1778fd5b628e3737c22acd74cc8c0b489a
luampuero <ampueroluana@gmail.com>
2024-02-25 21:25:49 -0300

    revisando update del producto

--- avalanna/src/controllers/productsController.js
+++ avalanna/src/controllers/productsController.js
@@ -1,7 +1,7 @@
 const db = require("../database/models");
 const fs = require("fs");
 const path = require("path");
-const {getJson, setJson} = require("../utility/jsonMethod");
+const { getJson, setJson } = require("../utility/jsonMethod");
 const { Console } = require("console");
 
 
@@ -18,29 +18,29 @@ const productsController = {
         res.render("products/crear-formulario", { title: "formulario", user: req.session.user })
     },
 
-    store:(req,res) =>{
-    	const producto = req.body;
-		producto.id = Date.now();
+    store: (req, res) => {
+        const producto = req.body;
+        producto.id = Date.now();
         producto.image = req.file.filename;
-		const products = getJson("products.json")
-		products.push(producto)
-		
+        const products = getJson("products.json")
+        products.push(producto)
 
-	   setJson(products,"products.json")
-		res.redirect(`/products`);
+
+        setJson(products, "products.json")
+        res.redirect(`/products`);
 
     },
 
 
-    edform:(req,res) => {
-        const {id}= req.params;
-        console.log("mostrar id edform",id)
+    edform: (req, res) => {
+        const { id } = req.params;
+        console.log("mostrar id edform", id)
         const products = getJson("products.json")
         const product = products.find(elemento => elemento.id == id);
-        res.render("products/edform", {title:"edform", product, user: req.session.user})
+        res.render("products/edform", { title: "edform", product, user: req.session.user })
     },
-    update:(req,res) =>{
-        console.log("file:",req.file); 
+    update: (req, res) => {
+        console.log("file:", req.file);
         /* Para multiples imagenes:
         const images = [];
         if(req.files){
@@ -49,19 +49,19 @@ const productsController = {
             }); 
         }
     */
-        const {id}=req.params;
-        console.log("mostrar id",id)
-        const {image, name, price, discount, description, extraDescription, height, width, depth, category} = req.body;
+        const { id } = req.params;
+        console.log("mostrar id", id)
+        const { image, name, price, discount, description, extraDescription, height, width, depth, category } = req.body;
         const products = getJson("products.json")
-        console.log("products...",products)
-        const newArray = products.map(product=>{
+        console.log("products...", products)
+        const newArray = products.map(product => {
             if (product.id == id) {
-                return{
+                return {
                     id,
                     image: req.file ? req.file.filename : product.image,
                     name,
-                    price:+price,
-                    discount:+discount,
+                    price: +price,
+                    discount: +discount,
                     description,
                     extraDescription,
                     height,
@@ -72,12 +72,12 @@ const productsController = {
             }
             return product
         });
-        console.log("ESTO es newArray",newArray)
+        console.log("ESTO es newArray", newArray)
         setJson(newArray, "products.json");
         res.redirect(`/products/detail/${id}`)
 
 
-        
+
     },
     cart: (req, res) => {
         res.render("products/productCart", { title: "Carrito de compra", user: req.session.user });
@@ -88,45 +88,82 @@ const productsController = {
         res.render("products/dashboard", { title: "Dashboard", products, propiedades, user: req.session.user })
     },
 
-    delete:(req,res)=>{
-        const {id}=req.params;
-        const productos=getJson("products.json");
-        const nuevaLista=productos.filter(elemento => elemento.id != id);
+    delete: (req, res) => {
+        const { id } = req.params;
+        const productos = getJson("products.json");
+        const nuevaLista = productos.filter(elemento => elemento.id != id);
         setJson(nuevaLista, "products.json");
         res.redirect("/products/dashboard");
     },
 
-    destroy:(req,res)=>{
-        const {id}=req.params;
-        const products=getJson("products.json");
-        const product=products.find(producto => producto.id == id);
-        const nuevoArray=products.filter(producto => producto.id != id );
-        console.log("imagen:",product.image);
-        fs.unlink(path.join(__dirname,`../../public/img/${product.image}`),(err)=>{
-            if(err) throw err;
+    destroy: (req, res) => {
+        const { id } = req.params;
+        const products = getJson("products.json");
+        const product = products.find(producto => producto.id == id);
+        const nuevoArray = products.filter(producto => producto.id != id);
+        console.log("imagen:", product.image);
+        fs.unlink(path.join(__dirname, `../../public/img/${product.image}`), (err) => {
+            if (err) throw err;
             console.log(`archivo ${product.image}`);
         })
-        setJson(nuevoArray,"products.json")
+        setJson(nuevoArray, "products.json")
         res.redirect(`/products`);
 
-       
+
     },
-    products:(req,res) =>{
+    products: (req, res) => {
         const products = getJson("products.json");
-        res.render("products/products", {title: "Todos los productos", products, user: req.session.user});
+        res.render("products/products", { title: "Todos los productos", products, user: req.session.user });
     },
-    categories:(req,res)=>{
-        const {category} = req.params;
+    categories: (req, res) => {
+        const { category } = req.params;
         const products = getJson("products.json");
-        const productsCategorized = products.filter(product=>{
+        const productsCategorized = products.filter(product => {
             return product.category == category.toLowerCase()
         });
-        res.render("products/categories", {title: category, productsCategorized, category, user: req.session.user})
-    }
+        res.render("products/categories", { title: category, productsCategorized, category, user: req.session.user })
+    },
+
+    // create:(req, res) => {
+    //     const producto = req.body;
+    //     producto.id = uuidv4();
+    //     const products = getJson("products");
+    //     products.push(producto);
+    //     setJson(products,"products");
+    //     res.redirect("/products/dashboard");
 
 
+    update: (req, res) => {
+        const { text, category, height, width, depth, imagen, sticker } = req.body;
+        db.User.update(
 
+
+            productsModify = products.map(producto => {
+                if (producto.id == id) {
+                    return {
+                        id,
+                        name: text,
+                        precio: text,
+                        descuento: text,
+                        descripcion: text,
+                        extraDescripcion: text,
+                        categoria: category,
+                        medidas: height, width, depth,
+                        imagen: (imagen ? imagen : producto.imagen),
+                        sticker
+                    }
+                }
+                return producto
+           
+            }))
+
+        setJson(productsModify, "products");
+
+        res.redirect(`/products/detail/${id}`);
+    },
+ 
 }
 
 
-module.exports = ( productsController )
+
+module.exports = (productsController)
--- avalanna/src/controllers/usersController.js
+++ avalanna/src/controllers/usersController.js
@@ -1,123 +1,150 @@
 const db = require("../database/models");
-const {setJson,getJson} = require("../utility/jsonMethod");
+const { setJson, getJson } = require("../utility/jsonMethod");
 const { v4: uuidv4 } = require('uuid');
 const bcrypt = require('bcryptjs');
-const {validationResult} = require('express-validator');
+const { validationResult } = require('express-validator');
 
 const usersController = {
-    login: (req,res)=>{
-        res.render("users/login", {title:"Ingresar"});
-    },
-    processlogin: (req, res) => {
-      const errors = validationResult(req);
-      if(!errors.isEmpty()){
-        console.log(errors)
-        res.render("users/login",{errors: errors.mapped(), title:"avalanna" , old:req.body});
-      }
-      const {email} = req.body;
-      const users = getJson("users.json");
-      const user = users.find(usuario => usuario.email == email);
-     
-      req.session.user = user;
-      delete user.password1;
-        if (req.body.rememberMe) {
-          res.cookie('userEmail',user.email,{maxAge: 1000 * 60 * 15 });
-          res.cookie('rememberMe',"true", {maxAge: 1000 * 60 * 15 });
-          console.log(req.cookies, "estas son las cookies");
-        }
-        res.redirect('/');
-    },
-      
-    
- 
-    register:(req,res)=>{
-        res.render("users/register", {title:"Registrarme", user: req.session.user});
-    },
-    createUser: (req, res) => {
-      const errores = validationResult(req);
-      console.log("errores:", errores);
-      console.log("body:",req.body);
+  login: (req, res) => {
+    res.render("users/login", { title: "Ingresar" });
+  },
+  processlogin: (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(errors)
+      res.render("users/login", { errors: errors.mapped(), title: "avalanna", old: req.body });
+    }
+    const { email } = req.body;
+    const users = getJson("users.json");
+    const user = users.find(usuario => usuario.email == email);
 
-      if(!errores.isEmpty()){
-        console.log("ingrese en errores");
-        res.render("users/register",{errores:errores.mapped(),old:req.body,title:"registro"})
-      }
-      else{ 
+    req.session.user = user;
+    delete user.password1;
+    if (req.body.rememberMe) {
+      res.cookie('userEmail', user.email, { maxAge: 1000 * 60 * 15 });
+      res.cookie('rememberMe', "true", { maxAge: 1000 * 60 * 15 });
+      console.log(req.cookies, "estas son las cookies");
+    }
+    res.redirect('/');
+  },
+
+
+
+  register: (req, res) => {
+    res.render("users/register", { title: "Registrarme", user: req.session.user });
+  },
+  createUser: (req, res) => {
+    const errores = validationResult(req);
+    console.log("errores:", errores);
+    console.log("body:", req.body);
+
+    if (!errores.isEmpty()) {
+      console.log("ingrese en errores");
+      res.render("users/register", { errores: errores.mapped(), old: req.body, title: "registro" })
+    }
+    else {
       const users = getJson("users.json");
-      const {NameAndSurname,email,phoneNumber,password1,rol} = req.body;
+      const { NameAndSurname, email, phoneNumber, password1, rol } = req.body;
       const id = uuidv4();
       const user = {
         id,
-        NameAndSurname:NameAndSurname,
-        email:email,
-        phoneNumber:phoneNumber,
-        password: bcrypt.hashSync(password1,10),
+        NameAndSurname: NameAndSurname,
+        email: email,
+        phoneNumber: phoneNumber,
+        password: bcrypt.hashSync(password1, 10),
         rol: rol ? rol : "user"
       }
       console.log(user);
       users.push(user);
-      setJson(users,"users.json");
+      setJson(users, "users.json");
       res.redirect('/users/ingresar');
     }
-},
-    edform:(req,res)=>{
-        const {id} = req.params;
-        const users = getJson("users.json");
-        const user = users.find(elemento => elemento.id == id);
-        res.render('users/actualizar-datos-usuario', { title: 'Editar', user, usuario:req.session.user});
-      },
-      update:(req,res)=>{
-        const errores = validationResult(req);
-      console.log("errores:", errores);
-      console.log("body:",req.body);
+  },
+  edform: (req, res) => {
+    const { id } = req.params;
+    const users = getJson("users.json");
+    const user = users.find(elemento => elemento.id == id);
+    res.render('users/actualizar-datos-usuario', { title: 'Editar', user, usuario: req.session.user });
+  },
+  update: (req, res) => {
+    const errores = validationResult(req);
+    console.log("errores:", errores);
+    console.log("body:", req.body);
 
-      if(!errores.isEmpty()){
-        console.log("ingrese en errores");
-        const {id} = req.params;
-        const users = getJson("users.json");
-        const user = users.find(elemento => elemento.id == id);
-        res.render("users/actualizar-datos-usuario",{errores:errores.mapped(),old:req.body,title:"editar",usuario:req.session.user, user})
-      }
-      else{ 
-        const {id} = req.params;
-        const {NameAndSurname,email,age,phoneNumber,date,rol} = req.body;
-        const users = getJson("users.json");
-        const usuarios = users.map(element => {
-          if (element.id == id) {
-            return {
-              id,
-              NameAndSurname,
-              email,
-              phoneNumber,
-              age,
-              date,
-              image: req.file ? req.file.filename : element.image, 
-              password: element.password,
-              rol: rol ? rol : "user"
-            }
+    if (!errores.isEmpty()) {
+      console.log("ingrese en errores");
+      const { id } = req.params;
+      const users = getJson("users.json");
+      const user = users.find(elemento => elemento.id == id);
+      res.render("users/actualizar-datos-usuario", { errores: errores.mapped(), old: req.body, title: "editar", usuario: req.session.user, user })
+    }
+    else {
+      const { id } = req.params;
+      const { NameAndSurname, email, age, phoneNumber, date, rol } = req.body;
+      const users = getJson("users.json");
+      const usuarios = users.map(element => {
+        if (element.id == id) {
+          return {
+            id,
+            NameAndSurname,
+            email,
+            phoneNumber,
+            age,
+            date,
+            image: req.file ? req.file.filename : element.image,
+            password: element.password,
+            rol: rol ? rol : "user"
           }
-          return element
-        });
-        setJson(usuarios,"users.json");
-        const userUpdate = usuarios.find(elemento => elemento.id == id);
-        req.session.user = userUpdate;
-        delete userUpdate.password
-        res.cookie('user',userUpdate)
-        res.redirect(`/users/editar/${id}`);
-      }},
-
-      dashboard:(req,res)=>{
-        res.send(req.session.user)
-      },
-      logout:(req,res) =>{
-        req.session.destroy();
-        console.log("estas son las cookies", req.cookies);
-        if (req.cookies) {
-          res.clearCookie('user');
-          res.clearCookie("userEmail")
-          res.clearCookie('rememberMe');
         }
-        res.redirect('/');
-}
+        return element
+      });
+      setJson(usuarios, "users.json");
+      const userUpdate = usuarios.find(elemento => elemento.id == id);
+      req.session.user = userUpdate;
+      delete userUpdate.password
+      res.cookie('user', userUpdate)
+      res.redirect(`/users/editar/${id}`);
+    }
+  },
+
+  dashboard: (req, res) => {
+    res.send(req.session.user)
+  },
+  logout: (req, res) => {
+    req.session.destroy();
+    console.log("estas son las cookies", req.cookies);
+    if (req.cookies) {
+      res.clearCookie('user');
+      res.clearCookie("userEmail")
+      res.clearCookie('rememberMe');
+    }
+    res.redirect('/');
+  },
+
+  processUpdate: (req, res) => {
+    const { id } = req.params;
+    const {name, price, description,extradescription, discount, image} = req.body;
+    db.products.update(
+      {
+        name: name,
+        price: price ,
+        description: description ,
+        extradescripcion: extradescription ,
+        discount: discount ,
+        imagen: image 
+      },
+      {
+        where: {
+          id,
+        },
+      }
+    )
+      .then((resp) => {
+        res.redirect("/products/detail/${id}");
+      })
+      .catch((err) => console.log(err));
   }
+
+
+}
 module.exports = usersController;
