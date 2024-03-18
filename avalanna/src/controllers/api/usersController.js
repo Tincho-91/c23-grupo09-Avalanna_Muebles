dfc502a2587794c2a1c3cc6ea67148277e121572
MelanyStrack <melany.strack@gmail.com>
2024-03-18 18:42:03 -0300

    Cree el controller de edición de usuarios para api

--- avalanna/src/controllers/api/usersController.js
+++ avalanna/src/controllers/api/usersController.js
@@ -1,38 +1,50 @@
-const db = require("../database/models");
+const db = require("../../database/models");
 const { op } =require("sequelize");
 const { validationResult } = require("express-validator");
 const path = require("path");
+const bcrypt = require('bcryptjs');
 
 module.exports = {
         update: async (req,res)=>{
             console.log("req.body: ", req.body);
             try {
+                const errors = validationResult(req);
                 const id = parseInt(req.params.id);
                 if (!Number.isInteger(id)) {
                     throw new Error (`"${id}" no es un término válido, debe ingresar un número entero`)
                 }
-    
-                const user = await db.User.findByPk(id);
-                console.log("req.body: ", req.body);
-    
-                if (!user) {
-                    throw new Error (`Usuario inexistente`);
-                }
+
+                if (errors.isEmpty()) {
+                    const user = await db.User.findByPk(id);
+                    if (!user) {
+                        throw new Error (`Usuario inexistente`);
+                    }
+                    
                 const {nameAndSurname, email, phoneNumber, password, rolId, birthday} = req.body;
-                    await user.update({
-                      nameAndSurname,
-                      email,
-                      phoneNumber,
-                      password: password ? password.hashSync() : user.password,
-                      rolId,
-                      birthday,
-                      profileImage: user.profileImage ? user.profileImage : "default.jpg"
-                    })
-    
-                    return res.status(200).json(user);
-                
+                await user.update({
+                  nameAndSurname,
+                  email,
+                  phoneNumber,
+                  password: password ? bcrypt.hashSync(password,10) : user.password,
+                  rolId,
+                  birthday,
+                  profileImage: user.profileImage ? user.profileImage : "default.jpg"
+                })
+
+                return res.status(200).json(user);
+                }else{
+                    const errorsMapped = errors.mapped()
+                    for (const key in errorsMapped){
+                        delete errorsMapped[key].type;
+                        delete errorsMapped[key].location;
+                        delete errorsMapped[key].path;
+                    }
+
+                    throw new Error (errorsMapped)
+                }
+
             } catch (error) {
-                return res.status(400).send(error.message);
+                return res.status(400).json(error.message);
             }
         }
     }
