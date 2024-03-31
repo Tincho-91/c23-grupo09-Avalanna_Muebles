af64b11d070fa587c07fe8af1be9802e4b4752e1
Tincho-91 <martin_baryuk@outlook.com>
2024-03-31 16:27:53 -0300

    probando validar login

---
+++ avalanna/public/js/login.js
@@ -0,0 +1,158 @@
+/*window.addEventListener("load", function () {
+
+    const inputs = document.querySelectorAll("input")
+    const form = document.querySelector("form")
+    const pErrors = document.querySelectorAll(".erroresForm");
+    const divs = document.querySelectorAll("input")
+    const select = document.querySelector("select")
+
+    const addErrorP = function(element){
+        const errorP = document.createElement("p");
+        errorP.classList.add(`error-${element.name}`)
+        errorP.classList.add("erroresForm")
+        const labelContent = document.querySelector(`.login_main_section-input-${element.name} label`).textContent
+        errorP.innerText=`"${labelContent.toUpperCase()}" no puede estar vacío`
+        divs.forEach(div=>{
+            const oldErrorP= document.querySelector(`.error-${element.name}`)
+            const rightDiv = document.querySelector(`.login_main_section-input-${element.name}`)
+            oldErrorP ? rightDiv.replaceChild(errorP, oldErrorP) : rightDiv.appendChild(errorP)
+        })
+        element.style.border= "2px solid red"
+        element.style.backgroundColor = "rgba(255,0,0,10%)"
+    }
+
+    const deleteError = function(element){
+        const childP = document.querySelector(`.error-${element.name}`)
+        const divFather = document.querySelector(`.login_main_section-input-${element.name}`)        
+        childP ? divFather.removeChild(childP) : null
+        element.style.backgroundColor= "var(--color-beige)"
+                element.style.border = "1px solid black"
+    }
+
+    const min = function (value, num){
+        return value.length >= num
+    }
+
+    const validation = function(element, e){
+        if (element.value == "") {
+            console.log("element", element.value);
+            addErrorP(element)
+        }else{
+            deleteError(element)
+        }
+        
+        if (element.name == "email") {
+            if (!min(element.value, 5)) {
+                console.log("ACA",min(element.value, 5));
+                addErrorP(element)
+                const labelContent = document.querySelector(`.login_main_section-input-${element.name} label`).textContent
+                document.querySelector(`.error-${element.name}`).innerText = `"${labelContent.toUpperCase()}" debe tener un mínimo de 5 caracteres`
+            }else{
+                deleteError(element)
+            }
+        }
+
+        if (element.name == "password1") {
+            if (!min(element.value, 20)) {
+                addErrorP(element)
+                const labelContent = document.querySelector(`.login_main_section-input-${element.name} label`).textContent
+                document.querySelector(`.error-${element.name}`).innerText = `"${labelContent.toUpperCase()}" debe tener un mínimo de 20 caracteres`
+            }else if (element.value == "") {
+                addErrorP(element)
+            }else{
+                deleteError(element)
+            }
+        }
+
+    }
+
+    inputs.forEach(input => {
+        input.addEventListener("blur", function(e){
+            validation(this,e)
+        })
+    });
+    select.addEventListener("blur", function(e){
+        validation(select)
+    })
+
+})*/
+window.addEventListener("load", function () {
+
+    const inputs = document.querySelectorAll("input")
+    const form = document.querySelector("form")
+    const pErrors = document.querySelectorAll(".erroresForm");
+    const divs = document.querySelectorAll("div")
+    const select = document.querySelector("select")
+
+    const addErrorP = function(element){
+        const errorP = document.createElement("p");
+        errorP.classList.add(`error-${element.name}`)
+        errorP.classList.add("erroresForm")
+        const labelContent = document.querySelector(`.form_main_section-div-${element.name} label`).textContent
+        errorP.innerText=`"${labelContent.toUpperCase()}" no puede estar vacío`
+        divs.forEach(div=>{
+            const oldErrorP= document.querySelector(`.error-${element.name}`)
+            const rightDiv = document.querySelector(`.form_main_section-div-${element.name}`)
+            oldErrorP ? rightDiv.replaceChild(errorP, oldErrorP) : rightDiv.appendChild(errorP)
+        })
+        element.style.border= "2px solid red"
+        element.style.backgroundColor = "rgba(255,0,0,10%)"
+    }
+
+    const deleteError = function(element){
+        const childP = document.querySelector(`.error-${element.name}`)
+        const divFather = document.querySelector(`.form_main_section-div-${element.name}`)        
+        childP ? divFather.removeChild(childP) : null
+        element.style.backgroundColor= "var(--color-beige)"
+                element.style.border = "1px solid black"
+    }
+
+    const min = function (value, num){
+        return value.length >= num
+    }
+
+    const validation = function(element, e){
+        if (element.value == "") {
+            console.log("element", element.value);
+            addErrorP(element)
+        }else{
+            deleteError(element)
+        }
+        
+        if (element.name == "email") {
+            if (!min(element.value, 5)) {
+                console.log("ACA",min(element.value, 5));
+                addErrorP(element)
+                const labelContent = document.querySelector(`.form_main_section-div-${element.name} label`).textContent
+                document.querySelector(`.error-${element.name}`).innerText = `"${labelContent.toUpperCase()}" debe tener un mínimo de 5 caracteres`
+            }else{
+                deleteError(element)
+            }
+        }
+
+        if (element.name == "password1") {
+            if (!min(element.value, 20)) {
+                addErrorP(element)
+                const labelContent = document.querySelector(`.form_main_section-div-${element.name} label`).textContent
+                document.querySelector(`.error-${element.name}`).innerText = `"${labelContent.toUpperCase()}" debe tener un mínimo de 20 caracteres`
+            }else if (element.value == "") {
+                addErrorP(element)
+            }else{
+                deleteError(element)
+            }
+        }
+
+    
+
+    }
+
+    inputs.forEach(input => {
+        input.addEventListener("blur", function(e){
+            validation(this,e)
+        })
+    });
+    select.addEventListener("blur", function(e){
+        validation(select)
+    })
+
+})
