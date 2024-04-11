29bbf81cff1c73a8b242129117eaa7e7237c377e
MelanyStrack <melany.strack@gmail.com>
2024-04-11 16:54:04 -0300

    Creación de proyecto en React y estructura de archivos

--- avalanna/react-admin/src/components/SideBar.jsx
+++ avalanna/react-admin/src/components/SideBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import image from '../assets/images/logo-DH.png';
+import image from '../assets/images/Imagotipo.png';
 import ContentWrapper from './ContentWrapper';
 import GenresInDb from './GenresInDb';
 import LastMovieInDb from './LastMovieInDb';
@@ -16,7 +16,7 @@ function SideBar(){
                 {/*<!-- Sidebar - Brand -->*/}
                 <a className="sidebar-brand d-flex align-items-center justify-content-center" href="/">
                     <div className="sidebar-brand-icon">
-                        <img className="w-100" src={image} alt="Digital House"/>
+                        <img className="w-100" src={image} alt="Avalanna Muebles"/>
                     </div>
                 </a>
 
@@ -27,7 +27,7 @@ function SideBar(){
                 <li className="nav-item active">
                     <Link className="nav-link" to="/">
                         <i className="fas fa-fw fa-tachometer-alt"></i>
-                        <span>Dashboard - DH movies</span></Link>
+                        <span>Dashboard - Avalanna Muebles</span></Link>
                 </li>
 
                 {/*<!-- Divider -->*/}
--- avalanna/react-admin/src/main.jsx
+++ avalanna/react-admin/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
-import './assets/css/app.css'
+import './assets/css/style.css'
+
 
 import {BrowserRouter} from 'react-router-dom';
 
